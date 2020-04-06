c13365b7f632474fe66038641d083709a16f9922
d.bondarev <d.bondarev@crm-onebox.com>
2020-04-06 16:34:48 +0300

    search part1

---
+++ tools/3-generate-search-list.php
@@ -0,0 +1,36 @@
+<?php
+class Tool_Example {
+
+    public function process() {
+        $files = $this->getDirContents('/home/devel003/uewiki/Traine9.github.io/Unreal Engine Wiki/mediawikiv2-website-prod05.ol.epicgames.net');
+        $pathArray = [];
+        foreach ($files as $path) {
+            if (!in_array(pathinfo($path, PATHINFO_EXTENSION), ['htm', 'html'])) {
+                continue;
+            }
+            $content = file_get_contents($path);
+            $result = preg_match('/<h1[^>]+id="firstHeading"[^>]+>(.*)<\/h1>/u', $content, $r);
+            $title = @$r[1];
+            $title = trim($title);
+            $title = htmlspecialchars_decode($title);
+            if (!$result || $title == 'We\'re working on a new Wiki!') {
+                continue;
+            }
+            $pathArray[$title] = array(
+                'path' => $path,
+                'title' => $title
+            );
+        };
+    }
+    public function getDirContents($dir) {
+        $path = array();
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+        foreach ($iterator as $file) {
+            if ($file->isDir()) continue;
+            $path[] = $file->getPathname();
+        }
+        return $path;
+    }
+}
+$x = new Tool_Example();
+$x->process();
---
+++ tools/4-replace-search.php
@@ -0,0 +1,29 @@
+<?php
+class Tool_Example {
+
+    public function process() {
+        $files = $this->getDirContents('/home/devel003/uewiki/Traine9.github.io/Unreal Engine Wiki/mediawikiv2-website-prod05.ol.epicgames.net');
+        $pathArray = [];
+        foreach ($files as $path) {
+            if (!in_array(pathinfo($path, PATHINFO_EXTENSION), ['htm', 'html'])) {
+                continue;
+            }
+            $content = file_get_contents($path);
+            $content = preg_replace('/<form[^>]*id="searchform"[^>]*>/u', '<form id="searchform" action="search.html" method="GET">', $content);
+            if ($content) {
+                file_put_contents($content, $path);
+            }
+        };
+    }
+    public function getDirContents($dir) {
+        $path = array();
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+        foreach ($iterator as $file) {
+            if ($file->isDir()) continue;
+            $path[] = $file->getPathname();
+        }
+        return $path;
+    }
+}
+$x = new Tool_Example();
+$x->process();
